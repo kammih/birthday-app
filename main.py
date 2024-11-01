66a31519f394f442fe5492123dff8b8b2230c057
Kammi Henderson <kammi_henry@yahoo.com>
2024-11-01 14:21:10 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 user_input = input("Which birthday are you looking for? ")
+
+if user_input == "Rubi":
+    print("Birthday is 03/13/23!")
+elif user_input == "Tyler":
+    print( "Birthday is 01/03/95!")
+elif user_input == "Truitt":
+    print("Birthday is 01/06/25!")
+else:
+    print("Can't find birthday for this person...")
